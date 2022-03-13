aa786bc7fb9e08aa63f63e1f3e883558bff24b37
aybe <aybe@users.noreply.github.com>
2022-03-14 00:22:25 +0100

    Make logging caller information optional

--- ProjectPSX.WPF/Logging/LoggingUtility.cs
+++ ProjectPSX.WPF/Logging/LoggingUtility.cs
@@ -6,20 +6,25 @@ namespace ProjectPSX.WPF.Logging;
 
 public static class LoggingUtility
 {
-    public static ILogger GetDefaultLogger()
+    public static ILogger GetDefaultLogger(bool includeCallerInformation = false)
     {
         // https://github.com/serilog/serilog/wiki/Formatting-Output
 
         const string currentOutputTemplate =
             "[{Timestamp:HH:mm:ss.fff}] {Level} {Message:lj} {Caller}{NewLine}";
 
-        var configuration = new LoggerConfiguration();
+        var configuration =
+            new LoggerConfiguration()
+                .MinimumLevel
+                .Verbose()
+                .WriteTo.Console(outputTemplate: currentOutputTemplate, theme: SystemConsoleTheme.Colored);
 
-        var logger = configuration
-            .MinimumLevel.Verbose()
-            .Enrich.WithCaller()
-            .WriteTo.Console(outputTemplate: currentOutputTemplate, theme: SystemConsoleTheme.Colored)
-            .CreateLogger();
+        if (includeCallerInformation)
+        {
+            configuration = configuration.Enrich.WithCaller();
+        }
+
+        var logger = configuration.CreateLogger();
 
         return logger;
     }
