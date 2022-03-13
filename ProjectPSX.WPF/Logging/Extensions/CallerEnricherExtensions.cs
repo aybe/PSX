964ae04c8d1b08f137bef81f8383622651498986
aybe <aybe@users.noreply.github.com>
2022-03-14 00:15:00 +0100

    Add nice console logging configuration

---
+++ ProjectPSX.WPF/Logging/Extensions/CallerEnricherExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace ProjectPSX.WPF.Logging.Extensions;
+
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+internal static class CallerEnricherExtensions
+{
+    public static LoggerConfiguration WithCaller(this LoggerEnrichmentConfiguration configuration)
+    {
+        if (configuration == null)
+            throw new ArgumentNullException(nameof(configuration));
+
+        return configuration.With<CallerEnricher>();
+    }
+
+    private sealed class CallerEnricher : ILogEventEnricher
+    {
+        private static Assembly SerilogAssembly { get; } = typeof(Log).Assembly;
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(new LogEventProperty("Caller", new ScalarValue(GetString() ?? "<unknown method>")));
+        }
+
+        private static string? GetString(int methodsToSkip = 2)
+        {
+            if (methodsToSkip <= 0)
+                throw new ArgumentOutOfRangeException(nameof(methodsToSkip));
+
+            var trace = new StackTrace(StackTrace.METHODS_TO_SKIP + methodsToSkip, true);
+
+            for (var i = 0; i < trace.FrameCount; i++)
+            {
+                var frame = trace.GetFrame(i);
+                if (frame == null)
+                    throw new InvalidOperationException($"Couldn't get stack frame at index {i}.");
+
+                var method = frame.GetMethod();
+                if (method == null)
+                    continue;
+
+                var type = method.DeclaringType;
+                if (type == null)
+                    continue;
+
+                if (type.Assembly == SerilogAssembly)
+                    continue;
+
+                var methodName = type.FullName;
+                var methodParams = string.Join(", ", method.GetParameters().Select(s => s.ParameterType.FullName));
+                var methodFile = frame.GetFileName();
+                var methodLine = frame.GetFileLineNumber();
+
+                return $"at {methodName}.{method.Name}({methodParams}) in {methodFile}:line {methodLine}";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ProjectPSX.WPF/Logging/LoggingUtility.cs
@@ -0,0 +1,26 @@
+﻿using ProjectPSX.WPF.Logging.Extensions;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace ProjectPSX.WPF.Logging;
+
+public static class LoggingUtility
+{
+    public static ILogger GetDefaultLogger()
+    {
+        // https://github.com/serilog/serilog/wiki/Formatting-Output
+
+        const string currentOutputTemplate =
+            "[{Timestamp:HH:mm:ss.fff}] {Level} {Message:lj} {Caller}{NewLine}";
+
+        var configuration = new LoggerConfiguration();
+
+        var logger = configuration
+            .MinimumLevel.Verbose()
+            .Enrich.WithCaller()
+            .WriteTo.Console(outputTemplate: currentOutputTemplate, theme: SystemConsoleTheme.Colored)
+            .CreateLogger();
+
+        return logger;
+    }
+}
