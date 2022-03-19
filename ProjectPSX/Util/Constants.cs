41b9dc0c604c87e14ef0ec91cd38c423fc2aa48d
aybe <aybe@users.noreply.github.com>
2022-03-19 22:24:57 +0100

    Move EX next to CPU

--- ProjectPSX/Util/Constants.cs
+++ ProjectPSX/Util/Constants.cs
@@ -1,16 +1,5 @@
 ﻿namespace ProjectPSX {
 
-    public enum EX {
-        INTERRUPT = 0x0,
-        LOAD_ADRESS_ERROR = 0x4,
-        STORE_ADRESS_ERROR = 0x5,
-        BUS_ERROR_FETCH = 0x6,
-        SYSCALL = 0x8,
-        BREAK = 0x9,
-        ILLEGAL_INSTR = 0xA,
-        COPROCESSOR_ERROR = 0xB,
-        OVERFLOW = 0xC
-    }
 
     public enum Width {
         WORD,
