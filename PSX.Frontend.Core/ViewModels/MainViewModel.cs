6f80ca53b526622543d739ecfe3e85048b21cbb8
aybe <aybe@users.noreply.github.com>
2022-04-01 21:53:36 +0200

    Open our other views using these interfaces

--- PSX.Frontend.Core/ViewModels/MainViewModel.cs
+++ PSX.Frontend.Core/ViewModels/MainViewModel.cs
@@ -2,21 +2,65 @@
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using PSX.Frontend.Core.Models;
+using PSX.Frontend.Core.Navigation;
+using PSX.Frontend.Core.Views;
 
 namespace PSX.Frontend.Core.ViewModels;
 
 public sealed class MainViewModel : ObservableRecipient
 {
-    public MainViewModel(MainModel model)
+    public MainViewModel(MainModel model, INavigationService navigationService)
     {
         Model = model;
+
+        NavigationService = navigationService;
 
         OpenFile = new RelayCommand(OpenFileExecute, OpenFileCanExecute);
 
         Shutdown = new RelayCommand(ShutdownExecute, ShutdownCanExecute);
+
+        OpenOutput = new RelayCommand(OpenOutputExecute, OpenOutputCanExecute);
+
+        OpenLogging = new RelayCommand(OpenLoggingExecute, OpenLoggingCanExecute);
     }
 
     private MainModel Model { get; }
+
+    private INavigationService NavigationService { get; }
+
+    #region OpenOutput
+
+    public RelayCommand OpenOutput { get; }
+
+    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
+    private bool OpenOutputCanExecute()
+    {
+        return true; // TODO this should either open or focus
+    }
+
+    private void OpenOutputExecute()
+    {
+        NavigationService.Navigate<IOutputView>();
+    }
+
+    #endregion
+
+    #region OpenLogging
+
+    public RelayCommand OpenLogging { get; }
+
+    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
+    private bool OpenLoggingCanExecute()
+    {
+        return true; // TODO this should either open or focus
+    }
+
+    private void OpenLoggingExecute()
+    {
+        NavigationService.Navigate<ILoggingView>();
+    }
+
+    #endregion
 
     #region OpenFile
 
