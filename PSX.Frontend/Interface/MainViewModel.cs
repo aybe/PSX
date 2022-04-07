c422bfd38c50253d75e59d657e452a8f188fe583
aybe <aybe@users.noreply.github.com>
2022-04-07 18:34:49 +0200

    Extract main model commands into its own class

--- PSX.Frontend/Interface/MainViewModel.cs
+++ PSX.Frontend/Interface/MainViewModel.cs
@@ -1,53 +1,25 @@
-﻿using System.Diagnostics.CodeAnalysis;
 ﻿using Microsoft.Extensions.Options;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
 using PSX.Frontend.Services.Emulation;
-using PSX.Frontend.Services.Navigation;
 
 namespace PSX.Frontend.Interface;
 
 public sealed class MainViewModel : ObservableRecipient
 {
     public MainViewModel(
-        MainModel model,
+        MainViewModelCommands commands,
         IEmulatorDisplayService emulatorDisplayService,
-        INavigationService navigationService,
         IOptions<AppSettings> appSettings)
     {
-        Model = model;
-
+        Commands               = commands;
         EmulatorDisplayService = emulatorDisplayService;
-
-        NavigationService = navigationService;
-
-        AppSettings = appSettings;
-
-        OpenFile = new RelayCommand(OpenFileExecute, OpenFileCanExecute);
-
-        OpenFileDirect = new RelayCommand<string>(OpenFileDirectExecute, OpenFileDirectCanExecute);
-
-        Shutdown = new RelayCommand(ShutdownExecute, ShutdownCanExecute);
-
-        OpenVideoScreen = new RelayCommand(OpenVideoScreenExecute, OpenVideoScreenCanExecute);
-
-        OpenVideoMemory = new RelayCommand(OpenVideoMemoryExecute, OpenVideoMemoryCanExecute);
-
-        EmulationStart    = new RelayCommand(EmulationStartExecute,    EmulationStartCanExecute);
-        EmulationStop     = new RelayCommand(EmulationStopExecute,     EmulationStopCanExecute);
-        EmulationPause    = new RelayCommand(EmulationPauseExecute,    EmulationPauseCanExecute);
-        EmulationFrame    = new RelayCommand(EmulationFrameExecute,    EmulationFrameCanExecute);
-        EmulationContinue = new RelayCommand(EmulationContinueExecute, EmulationContinueCanExecute);
         AppSettings            = appSettings;
     }
-
-    private MainModel Model { get; }
 
     private IEmulatorDisplayService EmulatorDisplayService { get; }
     public IOptions<AppSettings> AppSettings { get; }
 
-    private INavigationService NavigationService { get; }
-
+    public MainViewModelCommands Commands { get; }
 
     protected override void OnActivated()
     {
@@ -67,195 +39,4 @@ public sealed class MainViewModel : ObservableRecipient
     {
         // TODO
     }
-
-    #region EmulationStart
-
-    public RelayCommand EmulationStart { get; }
-
-    private bool EmulationStartCanExecute()
-    {
-        return Model.CanStart;
-    }
-
-    private void EmulationStartExecute()
-    {
-        Model.Start();
-
-        EmulationStart.NotifyCanExecuteChanged();
-        EmulationStop.NotifyCanExecuteChanged();
-        EmulationPause.NotifyCanExecuteChanged();
-        EmulationContinue.NotifyCanExecuteChanged();
-        EmulationFrame.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region EmulationStop
-
-    public RelayCommand EmulationStop { get; }
-
-    private bool EmulationStopCanExecute()
-    {
-        return Model.CanStop;
-    }
-
-    private void EmulationStopExecute()
-    {
-        Model.Stop();
-
-        EmulationStart.NotifyCanExecuteChanged();
-        EmulationStop.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region EmulationPause
-
-    public RelayCommand EmulationPause { get; }
-
-    private bool EmulationPauseCanExecute()
-    {
-        return Model.CanPause;
-    }
-
-    private void EmulationPauseExecute()
-    {
-        Model.Pause();
-
-        EmulationPause.NotifyCanExecuteChanged();
-        EmulationFrame.NotifyCanExecuteChanged();
-        EmulationContinue.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region EmulationFrame
-
-    public RelayCommand EmulationFrame { get; }
-
-    private bool EmulationFrameCanExecute()
-    {
-        return Model.CanFrame;
-    }
-
-    private void EmulationFrameExecute()
-    {
-        Model.Frame();
-
-        EmulationPause.NotifyCanExecuteChanged();
-        EmulationFrame.NotifyCanExecuteChanged();
-        EmulationContinue.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region EmulationContinue
-
-    public RelayCommand EmulationContinue { get; }
-
-    private bool EmulationContinueCanExecute()
-    {
-        return Model.CanContinue;
-    }
-
-    private void EmulationContinueExecute()
-    {
-        Model.Continue();
-
-        EmulationPause.NotifyCanExecuteChanged();
-        EmulationFrame.NotifyCanExecuteChanged();
-        EmulationContinue.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region OpenVideoScreen
-
-    public RelayCommand OpenVideoScreen { get; }
-
-    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
-    private bool OpenVideoScreenCanExecute()
-    {
-        return true;
-    }
-
-    private void OpenVideoScreenExecute()
-    {
-        NavigationService.Navigate<IVideoScreenView>();
-    }
-
-    #endregion
-
-    #region OpenVideoMemory
-
-    public RelayCommand OpenVideoMemory { get; }
-
-    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
-    private bool OpenVideoMemoryCanExecute()
-    {
-        return true;
-    }
-
-    private void OpenVideoMemoryExecute()
-    {
-        NavigationService.Navigate<IVideoMemoryView>();
-    }
-
-    #endregion
-
-    #region OpenFile
-
-    public RelayCommand OpenFile { get; }
-
-    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
-    private bool OpenFileCanExecute()
-    {
-        return true;
-    }
-
-    private void OpenFileExecute()
-    {
-        Model.OpenFile();
-        EmulationStart.NotifyCanExecuteChanged();
-        EmulationStop.NotifyCanExecuteChanged();
-        EmulationPause.NotifyCanExecuteChanged();
-        EmulationFrame.NotifyCanExecuteChanged();
-        EmulationContinue.NotifyCanExecuteChanged();
-    }
-
-    #endregion
-
-    #region OpenFileDirect
-
-    public RelayCommand<string> OpenFileDirect { get; }
-
-    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
-    private bool OpenFileDirectCanExecute(string? path)
-    {
-        return File.Exists(path);
-    }
-
-    private void OpenFileDirectExecute(string? path)
-    {
-        throw new NotImplementedException();
-    }
-
-    #endregion
-
-    #region Shutdown
-
-    public RelayCommand Shutdown { get; }
-
-    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Local")]
-    private bool ShutdownCanExecute()
-    {
-        return true;
-    }
-
-    private void ShutdownExecute()
-    {
-        Model.Shutdown();
-    }
-
-    #endregion
 }
---
+++ PSX.Frontend/Interface/MainViewModelCommands.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using PSX.Frontend.Services.Navigation;
+
+namespace PSX.Frontend.Interface;
+
+public sealed class MainViewModelCommands
+{
+    public MainViewModelCommands(MainModel model, INavigationService navigationService)
+    {
+        Shutdown = new RelayCommand(
+            model.Shutdown
+        );
+
+        OpenFile = new RelayCommand(
+            () =>
+            {
+                model.OpenFile();
+                NotifyCanExecuteChanged(EmuStart, EmuStop, EmuPause, EmuFrame, EmuContinue);
+            }
+        );
+
+        OpenFileDirect = new RelayCommand<string>(
+            path =>
+            {
+                // throw new NotImplementedException();
+
+                NotifyCanExecuteChanged(EmuStart, EmuStop, EmuPause, EmuFrame, EmuContinue);
+            },
+            File.Exists
+        );
+
+        ShowVideoScreen = new RelayCommand(
+            navigationService.Navigate<IVideoScreenView>
+        );
+
+        ShowVideoMemory = new RelayCommand(
+            navigationService.Navigate<IVideoMemoryView>
+        );
+
+        EmuStart = new RelayCommand(
+            () =>
+            {
+                model.Start();
+                NotifyCanExecuteChanged(EmuStart, EmuStop, EmuPause, EmuFrame, EmuContinue);
+            },
+            () => model.CanStart
+        );
+
+        EmuStop = new RelayCommand(
+            () =>
+            {
+                model.Stop();
+                NotifyCanExecuteChanged(EmuStart, EmuStop);
+            },
+            () => model.CanStop
+        );
+
+        EmuPause = new RelayCommand(
+            () =>
+            {
+                model.Pause();
+                NotifyCanExecuteChanged(EmuPause, EmuFrame, EmuContinue);
+            },
+            () => model.CanPause
+        );
+
+        EmuFrame = new RelayCommand(
+            () =>
+            {
+                model.Frame();
+                NotifyCanExecuteChanged(EmuPause, EmuFrame, EmuContinue);
+            },
+            () => model.CanFrame
+        );
+
+        EmuContinue = new RelayCommand(
+            () =>
+            {
+                model.Continue();
+                NotifyCanExecuteChanged(EmuPause, EmuFrame, EmuContinue);
+            },
+            () => model.CanContinue
+        );
+
+        static void NotifyCanExecuteChanged(params IRelayCommand?[] commands)
+        {
+            foreach (var command in commands)
+            {
+                command?.NotifyCanExecuteChanged();
+            }
+        }
+    }
+
+    public IRelayCommand EmuStart { get; }
+
+    public IRelayCommand EmuStop { get; }
+
+    public IRelayCommand EmuPause { get; }
+
+    public IRelayCommand EmuFrame { get; }
+
+    public IRelayCommand EmuContinue { get; }
+
+    public IRelayCommand ShowVideoScreen { get; }
+
+    public IRelayCommand ShowVideoMemory { get; }
+
+    public IRelayCommand OpenFile { get; }
+
+    public IRelayCommand<string> OpenFileDirect { get; }
+
+    public IRelayCommand Shutdown { get; }
+}
