f0aea33dd4cf6498a5e509ed4174b8dffb6b7894
aybe <aybe@users.noreply.github.com>
2022-04-10 02:43:12 +0200

    Create NullController.cs

---
+++ PSX.Devices.Input/NullController.cs
@@ -0,0 +1,18 @@
+﻿namespace PSX.Devices.Input;
+
+public sealed class NullController : ControllerBase
+{
+    private const int HiZ = 0xFF;
+
+    public override ushort Type { get; } = 0xFFFF;
+
+    public override void GenerateResponse()
+    {
+        base.GenerateResponse();
+
+        for (var i = 0; i < 15; i++)
+        {
+            TransferDataFifo.Enqueue(HiZ);
+        }
+    }
+}
