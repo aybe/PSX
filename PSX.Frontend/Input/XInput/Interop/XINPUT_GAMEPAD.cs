8ffb113a45894bd52420a501b2337f9ba8edf4e8
aybe <aybe@users.noreply.github.com>
2022-04-10 22:56:29 +0200

    Create NativeConstants.cs

---
+++ PSX.Frontend/Input/XInput/Interop/XINPUT_GAMEPAD.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace PSX.Frontend.Input.XInput.Interop;
+
+/// <summary>
+///     https://docs.microsoft.com/en-us/windows/win32/api/xinput/ns-xinput-xinput_gamepad
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+internal struct XINPUT_GAMEPAD
+{
+    public ushort wButtons;
+
+    public byte bLeftTrigger;
+
+    public byte bRightTrigger;
+
+    public short sThumbLX;
+
+    public short sThumbLY;
+
+    public short sThumbRX;
+
+    public short sThumbRY;
+}
