5701d39ffee67c79f0256c12db7a2ea44ae7c0aa
aybe <aybe@users.noreply.github.com>
2022-04-10 22:56:52 +0200

    Create NativeMethods.cs

---
+++ PSX.Frontend/Input/XInput/Interop/NativeMethods.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PSX.Frontend.Input.XInput.Interop;
+
+internal static class NativeMethods
+{
+    private const string DllName = "Xinput1_4.dll";
+
+    /// <summary>
+    ///     https://docs.microsoft.com/en-us/windows/win32/api/xinput/nf-xinput-xinputgetstate
+    /// </summary>
+    [DllImport(DllName)]
+    public static extern uint XInputGetState(
+        [In]           uint         dwUserIndex,
+        [In] [Out] ref XINPUT_STATE pState
+    );
+
+    /// <summary>
+    ///     https://docs.microsoft.com/en-us/windows/win32/api/xinput/nf-xinput-xinputsetstate
+    /// </summary>
+    [DllImport(DllName)]
+    public static extern uint XInputSetState(
+        [In]           uint             dwUserIndex,
+        [In] [Out] ref XINPUT_VIBRATION pVibration
+    );
+}
