a0b6fbadb4a84646704956e15f70d040c2b13ef9
aybe <aybe@users.noreply.github.com>
2022-04-10 22:56:38 +0200

    Create XINPUT_VIBRATION.cs

---
+++ PSX.Frontend/Input/XInput/Interop/XINPUT_VIBRATION.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace PSX.Frontend.Input.XInput.Interop;
+
+/// <summary>
+///     https://docs.microsoft.com/en-us/windows/win32/api/xinput/ns-xinput-xinput_vibration
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+internal struct XINPUT_VIBRATION
+{
+    public ushort wLeftMotorSpeed;
+
+    public ushort wRightMotorSpeed;
+}
