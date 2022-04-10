6d7d04ce71f7946257fd653a3942e00da39ccb92
aybe <aybe@users.noreply.github.com>
2022-04-10 19:17:17 +0200

    Create InputAction.cs

---
+++ PSX.Devices.Input/InputAction.cs
@@ -0,0 +1,29 @@
+﻿namespace PSX.Devices.Input;
+
+public enum InputAction
+{
+    DPadUp,
+    DPadDown,
+    DPadLeft,
+    DPadRight,
+    Triangle,
+    Circle,
+    Cross,
+    Square,
+    Select,
+    Start,
+    L1,
+    L2,
+    L3,
+    R1,
+    R2,
+    R3,
+    LeftX,
+    LeftY,
+    RightX,
+    RightY,
+    MouseL,
+    MouseR,
+    MouseX,
+    MouseY
+}
