38cb0ff8fe88aec88ee73f06339317cc2bdc4c7e
aybe <aybe@users.noreply.github.com>
2022-04-10 22:56:34 +0200

    Create XINPUT_STATE.cs

---
+++ PSX.Frontend/Input/XInput/Interop/XINPUT_STATE.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace PSX.Frontend.Input.XInput.Interop;
+
+/// <summary>
+///     https://docs.microsoft.com/en-us/windows/win32/api/xinput/ns-xinput-xinput_state
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+[SuppressMessage("ReSharper", "IdentifierTypo")]
+internal struct XINPUT_STATE
+{
+    public uint dwPacketNumber;
+
+    public XINPUT_GAMEPAD Gamepad;
+}
